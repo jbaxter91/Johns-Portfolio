eece842d76536282086a7939bfa460488dcc7a77
John Baxter <jbaxter@a-zdarts.com>
2020-12-19 10:59:02 -0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Home from './components/index';
-import Portfolio from './components/Portfolio';
-import {Route} from 'react-router-dom';
-import './App.css';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./components/index";
+import Portfolio from "./components/Portfolio";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <CssBaseline/>
-        <Route path="/" exact component={Home}/>
-        <Route path="/portfolio" exact component={Portfolio}/>
+      <CssBaseline />
+      <Route path="/" exact component={Home} />
+      <Route path="/portfolio" exact component={Portfolio} />
     </div>
   );
 }
