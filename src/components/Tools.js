80136eba7fffe50fb513612402ea9a2c3d4d86f2
John Baxter <jbaxter@a-zdarts.com>
2023-03-16 14:00:35 -0400

    adding text to tools page

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,16 +1,6 @@
 import React from "react";
 import { Box, Grid, Container } from "@material-ui/core";
 import Navbar from "./Navbar";
-import Projects from "./Projects";
-
-//Project Images
-import project1 from "../images/home-screen.jpg";
-import project2 from "../images/little-fish-go.jpg";
-import project3 from "../images/super-similar.jpg";
-import project4 from "../images/weather-dashboard.jpg";
-import project6 from "../images/supportx-2.jpg";
-import project7 from "../images/magic-sms.jpg";
-import project8 from "../images/a-zdarts.jpg";
 
 const Tools = () => {
   return (
@@ -18,7 +8,7 @@ const Tools = () => {
       <Navbar />
       <Container fixed style={{ marginTop: "2rem" }}>
         <Grid spacing={4} container justify="center">
-        
+        Hello
         </Grid>
       </Container>
     </Box>
